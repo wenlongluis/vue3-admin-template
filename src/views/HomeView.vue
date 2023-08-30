3f5c977aa84c12f9a6922f53df1d39a38a7d37db
liuwl9 <liuwl9@chinatelecom.cn>
2023-08-30 10:43:07 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <el-button>click me</el-button>
+    <TheWelcome />
+  </main>
+</template>
